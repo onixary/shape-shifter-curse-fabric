af5c2d1ef64c6c73357fd3f389c8591877f2025a
onixary <undecyce@gmail.com>
2025-09-07 11:01:42 +0800

    修复bat_3吸附动画在多人服务器未同步给其它玩家的问题 Fix issue which bat_3 attach animation not sync properly in multiplayer server

---
+++ src/main/java/net/onixary/shapeShifterCurseFabric/client/ClientPlayerStateManager.java
@@ -0,0 +1,37 @@
+package net.onixary.shapeShifterCurseFabric.client;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.onixary.shapeShifterCurseFabric.additional_power.BatBlockAttachPower;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ClientPlayerStateManager {
+    private static final Map<UUID, PlayerAttachState> PLAYER_ATTACH_STATES = new HashMap<>();
+
+    public static class PlayerAttachState {
+        public boolean isAttached;
+        public BatBlockAttachPower.AttachType attachType;
+        public BlockPos attachedPos;
+        public Direction attachedSide;
+    }
+
+    public static void updatePlayerAttachState(UUID playerUuid, boolean isAttached,
+                                               int attachTypeOrdinal, BlockPos attachedPos, Direction attachedSide) {
+        PlayerAttachState state = PLAYER_ATTACH_STATES.computeIfAbsent(playerUuid, k -> new PlayerAttachState());
+        state.isAttached = isAttached;
+        state.attachType = BatBlockAttachPower.AttachType.values()[attachTypeOrdinal];
+        state.attachedPos = attachedPos;
+        state.attachedSide = attachedSide;
+    }
+
+    public static PlayerAttachState getPlayerAttachState(UUID playerUuid) {
+        return PLAYER_ATTACH_STATES.get(playerUuid);
+    }
+
+    public static void clearPlayerState(UUID playerUuid) {
+        PLAYER_ATTACH_STATES.remove(playerUuid);
+    }
+}
