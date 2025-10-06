a642189aa884eb55e8ec2b36e78435bdfdb75eb3
xu233333 <18510478058@163.com>
2025-10-06 09:27:11 +0800

    创建形态选择Screen框架

---
+++ src/main/java/net/onixary/shapeShifterCurseFabric/custom_ui/PatronFormSelectScreen.java
@@ -0,0 +1,47 @@
+package net.onixary.shapeShifterCurseFabric.custom_ui;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormBase;
+import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormDynamic;
+import net.onixary.shapeShifterCurseFabric.player_form.RegPlayerForms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatronFormSelectScreen extends Screen {
+    private final ClientPlayerEntity player;
+
+    public PatronFormSelectScreen(Text title, ClientPlayerEntity player) {
+        super(title);
+        this.player = player;
+    }
+
+    private List<Identifier> getAvailableForms() {
+        List<Identifier> availableForms = new ArrayList<>();
+        for (Identifier formID : RegPlayerForms.dynamicPlayerForms) {
+            PlayerFormBase form = RegPlayerForms.getPlayerForm(formID);
+            if (form instanceof PlayerFormDynamic pfd) {
+                if (pfd.IsPatronForm && pfd.IsPlayerCanUse(player)) {
+                    if (!availableForms.contains(formID)) {
+                        availableForms.add(formID);
+                    }
+                }
+            }
+        }
+        return availableForms;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+    }
+}
