5cfc1c33b4a5dd044c28678cae925e704e972abe
xu233333 <18510478058@163.com>
2025-10-22 10:49:56 +0800

    重构动画控制器-填充AnimationStateCondition+修改顺序 后续可能还得调整顺序

--- src/main/java/net/onixary/shapeShifterCurseFabric/player_animation/AnimationControllerInstance.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/player_animation/AnimationControllerInstance.java
@@ -2,8 +2,12 @@ package net.onixary.shapeShifterCurseFabric.player_animation;
 
 import com.mojang.datafixers.util.Pair;
 import io.github.apace100.apoli.component.PowerHolderComponent;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.onixary.shapeShifterCurseFabric.ShapeShifterCurseFabric;
 import net.onixary.shapeShifterCurseFabric.additional_power.BatBlockAttachPower;
 import net.onixary.shapeShifterCurseFabric.client.ClientPlayerStateManager;
@@ -26,9 +30,82 @@ public class AnimationControllerInstance {
     public static Identifier AnimC_Power = AnimationController.RegisterAnimationStateCondition(
             ShapeShifterCurseFabric.identifier("anim_c_power"),
             (playerEntity, animDataHolder) -> true
-    );  // 所有形态都有可能有 如果在这里检查Power会带来没必要的性能开销
+    );  // 所有形态都有可能有 如果在这里检查Power会带来没必要的性能开销  如果没有Power则返回NOT_MATCH
 
-    // TODO 填充AnimationStateCondition注册
+    public static Identifier AnimC_Sleep = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_sleep"),
+            (playerEntity, animDataHolder) -> playerEntity.isSleeping()
+    );
+
+    public static Identifier AnimC_Ride = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_ride"),
+            (playerEntity, animDataHolder) -> playerEntity.hasVehicle()
+    );  // 坐船和骑乘共用
+
+    public static Identifier AnimC_Climb = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_climb"),
+            (playerEntity, animDataHolder) -> {
+                if (!playerEntity.isClimbing() || playerEntity.isOnGround() || playerEntity.getAbilities().flying || playerEntity.isFallFlying()) {
+                    return false;
+                }
+                // 检测碰撞箱 防止出现身体与地面穿模 如果卡顿可以直接可以修改为 return true
+                BlockPos down1pos = playerEntity.getBlockPos().down();
+                BlockState down1block = playerEntity.getWorld().getBlockState(down1pos);
+                Vec3d ClimbAnimTestPoint = playerEntity.getPos().add(0f, -0.6f, 0f);  // 检测点在身体中心下方0.6个方块是否有碰撞箱
+                BlockHitResult HitResult = down1block.getCollisionShape(playerEntity.getWorld(), down1pos).raycast(playerEntity.getPos(), ClimbAnimTestPoint, down1pos);
+                if (HitResult == null) { // 没有碰撞箱时
+                    return true;
+                }
+                else {
+                    return HitResult.getType() == BlockHitResult.Type.MISS;
+                }
+            }
+    );  // 是否爬行 从PlayerEntityAnimOverrideMixin中复制CanClimbAnim
+
+    public static Identifier AnimC_Swim = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_swim"),
+            (playerEntity, animDataHolder) -> playerEntity.isTouchingWater() && playerEntity.isSwimming()
+    );  // 没必要判断是否奔跑
+
+    public static Identifier AnimC_Flying = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_flying"),
+            (playerEntity, animDataHolder) -> playerEntity.getAbilities().flying
+    );  // 是否创造飞行
+
+    public static Identifier AnimC_FallFlying = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_fall_flying"),
+            (playerEntity, animDataHolder) -> playerEntity.isFallFlying()
+    );  // 是否鞘翅飞行
+
+    public static Identifier AnimC_Fall = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_fall"),
+            (playerEntity, animDataHolder) -> !playerEntity.isOnGround() && playerEntity.getVelocity().y < 0
+    );  // 与hasSlowFall合并 需要为Sneak单独实现
+
+    public static Identifier AnimC_Jump = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_jump"),
+            (playerEntity, animDataHolder) -> !playerEntity.isOnGround() && animDataHolder.lastOnGround && playerEntity.getVelocity().y > 0
+    );  // 与RushJump 合并 需要为Sneak单独实现
+
+    public static Identifier AnimC_UseItem = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_use_item"),
+            (playerEntity, animDataHolder) -> playerEntity.isUsingItem()
+    );  // 需要为Sneak单独实现
+
+    public static Identifier AnimC_Mining = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_mining"),
+            (playerEntity, animDataHolder) -> playerEntity.handSwinging && animDataHolder.continueSwingAnimCounter >= 10
+    );  // 需要为Sneak单独实现
+
+    public static Identifier AnimC_Attack = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_attack"),
+            (playerEntity, animDataHolder) -> playerEntity.handSwinging
+    );  // 由于已经在AnimC_UseTools中检查过，所以这里不需要检查continueSwingAnimCounter  需要为Sneak单独实现
+
+    public static Identifier AnimC_Walk = AnimationController.RegisterAnimationStateCondition(
+            ShapeShifterCurseFabric.identifier("anim_c_walk"),
+            (playerEntity, animDataHolder) -> animDataHolder.IsWalk
+    );  // 和奔跑合并 如果逻辑复杂 可以把奔跑分离  需要为Sneak单独实现
 
     public static void RegisterAnimCellToAllController(Identifier conditionID, BiFunction<PlayerEntity, AnimationController.PlayerAnimDataHolder, Pair<AnimationControllerCellResult, PlayerAnimState>> cell) {
         Controller_Normal.RegisterAnimControllerCell(conditionID, cell);
@@ -75,7 +152,7 @@ public class AnimationControllerInstance {
                     }
                 }
         );  // 从PlayerEntityAnimOverrideMixin 中复制过来的
-
+        // TODO 填充动作控制器Cell
 
     }
 }
