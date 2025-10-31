da74f729e9ea36ed3a48692c5815848ef2230024
xu233333 <18510478058@163.com>
2025-10-31 17:00:31 +0800

    LevitatePower应该是修改完美了 但是玩家连点空格会导致飞行高度比原先的高

--- src/main/java/net/onixary/shapeShifterCurseFabric/additional_power/LevitatePower.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/additional_power/LevitatePower.java
@@ -17,9 +17,7 @@ import net.onixary.shapeShifterCurseFabric.ShapeShifterCurseFabric;
 
 public class LevitatePower extends Power implements Active {
     // 配置参数
-    private float startAscentSpeed = 0.5f;
-    private float endAscentSpeed = 0.1f;
-    private int accelerationDuration = 20;
+    private float ascentSpeed = 0.5f;
     private int maxAscendDuration = 40;
     private Key key;
 
@@ -28,12 +26,7 @@ public class LevitatePower extends Power implements Active {
     private boolean wasActiveLastTick = false;
     private boolean isKeyActive = false;
     private boolean isLevitate = false;
-
-    private int accelerationProcess = 0;
-
-    private float getAscendSpeed() {
-        return startAscentSpeed + ((endAscentSpeed - startAscentSpeed) * ((float)accelerationProcess / accelerationDuration));
-    }
+    private boolean HasModifyUpVelocity = false;
 
     public LevitatePower(PowerType<?> type, LivingEntity entity) {
         super(type, entity);
@@ -44,56 +37,69 @@ public class LevitatePower extends Power implements Active {
     public void onUse() {
         if (entity instanceof PlayerEntity player) {
             //handleLevitationInput(player);
-            isKeyActive = true;
+            this.isKeyActive = true;
             PowerHolderComponent.syncPower(entity, this.type);
         }
     }
 
+    // 恢复ascendProgress
+    private void resetLevitateState() {
+        this.isLevitate = false;
+        this.ascendProgress = 0;
+        this.HasModifyUpVelocity = false;
+    }
 
+    // 当按键按下时的处理
+    private void processLevitate(PlayerEntity player) {
+        Vec3d velocity = player.getVelocity();
+        this.isLevitate = true;
+        if(this.ascendProgress < this.maxAscendDuration) {
+            player.setNoGravity(true);
+            if (!this.HasModifyUpVelocity) {
+                player.setVelocity(velocity.x, this.ascentSpeed, velocity.z);
+                player.velocityModified = true;
+                this.HasModifyUpVelocity = true;
+            }
+            this.ascendProgress++;
+        }
+        else {
+            player.setNoGravity(true);
+            if (velocity.y != 0) {
+                player.setVelocity(velocity.x, 0, velocity.z);
+                player.velocityModified = true;
+            }
+            this.HasModifyUpVelocity = false;
+        }
+    }
+
+    // 空中缓降
+    private void processStopLevitate(PlayerEntity player) {
+        this.isLevitate = false;
+        player.setNoGravity(false);
+        this.HasModifyUpVelocity = false;
+    }
 
     @Override
     public void tick() {
         if (entity instanceof PlayerEntity player) {
             if(player.getFluidHeight(FluidTags.WATER) > 0.0F || player.getFluidHeight(FluidTags.LAVA) > 0.0F){
-                isLevitate = false;
-                ascendProgress = 0;
+                this.resetLevitateState();
                 return;
             }
 
-            //ShapeShifterCurseFabric.LOGGER.info("IsAscending: " + isAscending);
-            if(isKeyActive){
-                isLevitate = true;
-                accelerationProcess++;
-                accelerationProcess = Math.min(accelerationProcess, accelerationDuration);
-                if(ascendProgress < maxAscendDuration){
-                    // player.setVelocity(player.getVelocity().x, 0, player.getVelocity().z);
-                    player.setNoGravity(true);
-                    player.setVelocity(player.getVelocity().x, this.getAscendSpeed(), player.getVelocity().z);
-                    player.velocityModified = true;
-                    ascendProgress ++;
-                    //ShapeShifterCurseFabric.LOGGER.info("Ascending Progress: " + ascendProgress);
-                }
-                else{
-                    player.setNoGravity(true);
-                    Vec3d velocity = player.getVelocity();
-                    if (velocity.y != 0) {
-                        player.setVelocity(velocity.x, 0, velocity.z);
-                        player.velocityModified = true;
-                    }
-                }
+            if(this.isKeyActive || this.wasActiveLastTick){
+                this.processLevitate(player);
             }
-            else if(!isKeyActive && !wasActiveLastTick){
-                accelerationProcess = 0;
-                isLevitate = false;
-                player.setNoGravity(false);
+            else {
+                this.processStopLevitate(player);
             }
+
             player.fallDistance = 0;
 
-            wasActiveLastTick = isKeyActive;
-            isKeyActive = false;
+            this.wasActiveLastTick = this.isKeyActive;
+            this.isKeyActive = false;
             if(entity.isOnGround()){
-                isLevitate = false;
-                ascendProgress = 0;
+                this.resetLevitateState();
             }
             PowerHolderComponent.syncPower(entity, this.type);
         }
@@ -126,17 +132,13 @@ public class LevitatePower extends Power implements Active {
         return new PowerFactory<>(
                 ShapeShifterCurseFabric.identifier("levitate"),
                 new SerializableData()
-                        .add("start_ascent_speed", SerializableDataTypes.FLOAT, 0.5f)
-                        .add("end_ascent_speed", SerializableDataTypes.FLOAT, 0.1f)
-                        .add("acceleration_duration", SerializableDataTypes.INT, 20)
+                        .add("ascent_speed", SerializableDataTypes.FLOAT, 0.5f)
                         .add("max_ascend_duration", SerializableDataTypes.INT, 40)
                         .add("key", ApoliDataTypes.BACKWARDS_COMPATIBLE_KEY, new Active.Key())
                         .add("hud_render", ApoliDataTypes.HUD_RENDER, HudRender.DONT_RENDER),
                 data -> (powerType, entity) -> {
                     LevitatePower power = new LevitatePower(powerType, entity);
-                    power.startAscentSpeed = data.getFloat("start_ascent_speed");
-                    power.endAscentSpeed = data.getFloat("end_ascent_speed");
-                    power.accelerationDuration = data.getInt("acceleration_duration");
+                    power.ascentSpeed = data.getFloat("ascent_speed");
                     power.maxAscendDuration = data.getInt("max_ascend_duration");
                     power.setKey(data.get("key"));
                     return power;
