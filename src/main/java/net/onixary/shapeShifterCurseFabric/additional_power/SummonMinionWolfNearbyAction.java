de03d2ecc8fbcc0d0ae42395c18b86ac7955b792
xu233333 <18510478058@163.com>
2025-11-23 10:36:27 +0800

    将summon_wolf_minion能力中的附近位置生成提成独立函数 并支持实体高度选项 bi_summon_wolf_minion 添加reverse配置 用于反转Owner和SpawnNearbyTarget 修复virtual_totem能力totem_status_effects实例相同的Bug 导致无法给予Buff效果 修复summon_wolf_minion的max_minion_count生效错误的问题

--- src/main/java/net/onixary/shapeShifterCurseFabric/additional_power/SummonMinionWolfNearbyAction.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/additional_power/SummonMinionWolfNearbyAction.java
@@ -8,47 +8,26 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.World;
 import net.onixary.shapeShifterCurseFabric.ShapeShifterCurseFabric;
 import net.onixary.shapeShifterCurseFabric.minion.IPlayerEntityMinion;
 import net.onixary.shapeShifterCurseFabric.minion.MinionRegister;
 import net.onixary.shapeShifterCurseFabric.minion.mobs.WolfMinion;
-import org.jetbrains.annotations.Nullable;
-
 
 public class SummonMinionWolfNearbyAction {
-    public static int RandomInt(Random random, int min, int max) {
-        return random.nextInt(max - min + 1) + min;
-    }
-
-    private static boolean IsEmptySpace(World world, BlockPos blockPos) {
-        return world.isAir(blockPos) || world.isWater(blockPos);
-    }
-
-    public static @Nullable BlockPos FindCanSpawn(World world, BlockPos blockPos) {
-        BlockPos nextPos = blockPos;
-        for (int i = 0; i < 4; i++) {
-            if (IsEmptySpace(world, nextPos)) {
-                if (!IsEmptySpace(world, nextPos.down())) {
-                    return nextPos;
-                }
-                nextPos = nextPos.down();
-            }
-        }
-        return null;
-    }
-
     public static void action(SerializableData.Instance data, Pair<Entity, Entity> entities) {
         Entity Owner = entities.getLeft();
         Entity SpawnNearbyTarget = entities.getRight();
+        if (data.isPresent("reverse") && data.getBoolean("reverse")) {
+            Owner = entities.getRight();
+            SpawnNearbyTarget = entities.getLeft();
+        }
         int MinionLevel = data.getInt("minion_level");
         int MinionCount = data.getInt("count");
         int MaxMinionCount = data.getInt("max_minion_count");
         if (Owner instanceof ServerPlayerEntity player) {
             for (int i = 0; i < MinionCount; i++) {
                 if (player instanceof IPlayerEntityMinion playerEntityMinion) {
-                    if (playerEntityMinion.shape_shifter_curse$getMinionsCount(WolfMinion.MinionID) > MaxMinionCount) {
+                    if (playerEntityMinion.shape_shifter_curse$getMinionsCount(WolfMinion.MinionID) >= MaxMinionCount) {
                         return;
                     }
                 }
@@ -56,17 +35,7 @@ public class SummonMinionWolfNearbyAction {
                     ShapeShifterCurseFabric.LOGGER.warn("Can't spawn minion, player is not IPlayerEntityMinion");
                     return;
                 }
-                BlockPos targetPos = null;
-                for (int nowTry = 0; nowTry < 4; nowTry++) {
-                    int j = RandomInt(player.getRandom(), -3, 3);
-                    int k = RandomInt(player.getRandom(), -1, 1);
-                    int l = RandomInt(player.getRandom(), -3, 3);
-                    targetPos = SpawnNearbyTarget.getBlockPos().add(j, k, l);
-                    targetPos = FindCanSpawn(SpawnNearbyTarget.getWorld(), targetPos);
-                    if (targetPos == null) {
-                        continue;
-                    }
-                }
+                BlockPos targetPos = MinionRegister.getNearbyEmptySpace(SpawnNearbyTarget.getWorld(), player.getRandom(), SpawnNearbyTarget.getBlockPos(), 3, 1, 1, 4);;
                 if (targetPos == null) {
                     targetPos = SpawnNearbyTarget.getBlockPos();
                 }
@@ -90,7 +59,8 @@ public class SummonMinionWolfNearbyAction {
                 new SerializableData()
                         .add("minion_level", SerializableDataTypes.INT, 1)
                         .add("count", SerializableDataTypes.INT, 1)
-                        .add("max_minion_count", SerializableDataTypes.INT, Integer.MAX_VALUE),
+                        .add("max_minion_count", SerializableDataTypes.INT, Integer.MAX_VALUE)
+                        .add("reverse", SerializableDataTypes.BOOLEAN, false),
                 SummonMinionWolfNearbyAction::action
         );
     }
