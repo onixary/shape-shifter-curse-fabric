0d658aedbb4e8179bdf07d424cd3b63ae0f7436f
xu233333 <18510478058@163.com>
2025-12-22 15:17:04 +0800

    由于1.20.1的Trinket没有对应的Event仅支持部分饰品对Power的修改

--- src/main/java/net/onixary/shapeShifterCurseFabric/mixin/TrinketItemMixin.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/mixin/TrinketItemMixin.java
@@ -2,6 +2,7 @@ package net.onixary.shapeShifterCurseFabric.mixin;
 
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
+import dev.emi.trinkets.api.TrinketItem;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -10,20 +11,17 @@ import net.minecraft.util.Identifier;
 import net.onixary.shapeShifterCurseFabric.util.TrinketUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // 本来想挂个Event 结果发现需要的Event 1.20.1 版本没有 所以只能用Mixin了 可能部分物品会失效 取决于是否使用super.onEquip等方法
-@Mixin(Trinket.class)
-public class TrinketInterfaceMixin {
+@Mixin(TrinketItem.class)
+public class TrinketItemMixin implements Trinket {
     @Unique
     public boolean CanAutoExecute(Identifier ItemID) {
         return TrinketUtils.getAccessoryMixinAuto(ItemID);
     }
 
-    @Inject(method = "onEquip", at = @At("HEAD"))
-    private void onEquipMixin(ItemStack stack, SlotReference slot, LivingEntity entity, CallbackInfo ci) {
+    @Override
+    public void onEquip(ItemStack stack, SlotReference slot, LivingEntity entity) {
         if (entity instanceof PlayerEntity player) {
             Identifier ItemID = Registries.ITEM.getId(stack.getItem());
             if (CanAutoExecute(ItemID)) {
@@ -32,8 +30,8 @@ public class TrinketInterfaceMixin {
         }
     }
 
-    @Inject(method = "onUnequip", at = @At("HEAD"))
-    private void onUnequipMixin(ItemStack stack, SlotReference slot, LivingEntity entity, CallbackInfo ci) {
+    @Override
+    public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
         if (entity instanceof PlayerEntity player) {
             Identifier ItemID = Registries.ITEM.getId(stack.getItem());
             if (CanAutoExecute(ItemID)) {
@@ -41,5 +39,4 @@ public class TrinketInterfaceMixin {
             }
         }
     }
-
 }
