91a5169a684af5364739b14f8fa8b39f2f9471ab
xu233333 <18510478058@163.com>
2026-01-13 10:51:07 +0800

    添加一个数据包用的魔力条类型 空Modifier 需要用Power添加修改上限和回复的Modifier

--- src/main/java/net/onixary/shapeShifterCurseFabric/mana/ManaRegistriesClient.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/mana/ManaRegistriesClient.java
@@ -27,6 +27,7 @@ public class ManaRegistriesClient {
 
     static {
         registerManaTypeRender(ManaRegistries.FAMILIAR_FOX_MANA, new FamiliarFoxManaBar());
+        registerManaTypeRender(ManaRegistries.DP_MANA, new InstinctBarLikeManaBar());
     }
 
     public static void register() {}
