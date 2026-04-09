4410342ca43c9bbf5b094356740dd06e21a457d0
xu233333 <18510478058@163.com>
2026-04-09 16:05:00 +0800

    看起来并不需要剩余时间的数显(没有自然恢复) 砍了

--- src/main/java/net/onixary/shapeShifterCurseFabric/mana/WebResourceBar.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/mana/WebResourceBar.java
@@ -56,30 +56,12 @@ public class WebResourceBar implements IManaRender {
         double mana = ManaUtils.getPlayerMana(mc.player);
         double maxMana = ManaUtils.getPlayerMaxMana(mc.player);
         double manaRegen = ManaUtils.getPlayerManaRegen(mc.player);
-        int remainTicks = -1;
-
-        if (manaRegen > (double)0.0F) {
-            remainTicks = (int)Math.ceil((maxMana - mana) / manaRegen);
-        } else if (manaRegen < (double)0.0F) {
-            remainTicks = (int)Math.ceil((mana) / (-manaRegen));
-        } else {
-            remainTicks = 0;
-        }
 
         int manaWidth = (int)Math.ceil((double)80.0F * ManaUtils.getManaPercent(mana, maxMana, (double)0.0F));
         context.drawTexture(BarTexID, x, y, 0.0f, 0, 80, 5, 80, 18);
         context.drawTexture(BarTexID, x, y, 0.0f, 5, manaWidth, 5, 80, 18);
-        StringBuilder manaString = new StringBuilder();
-        manaString.append((int)mana).append("/").append((int)maxMana);
-        if (remainTicks > 0) {
-            manaString.append(" (").append(remainTicks).append(")");
-        } else if (remainTicks < 0) {
-            manaString.append(" (").append("?").append(")");
-        } else {
-            manaString.append(" (").append("∞").append(")");
-        }
 
-        Text manaText = Text.literal(manaString.toString());
+        Text manaText = Text.literal((int) mana + "/" + (int) maxMana);
         context.drawText(mc.textRenderer, manaText, x + 10, y - 8, manaRegen == 0 ? 0xFF7F7F7F : 0xFF00CFFF, false);
 
         int chargeLevel = this.getChargeLevel();
