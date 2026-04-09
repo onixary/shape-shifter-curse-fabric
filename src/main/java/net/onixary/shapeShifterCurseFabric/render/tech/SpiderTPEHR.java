634bc9cd3fb3c2cf73063a694f610e37a7749bf8
onixary <undecyce@gmail.com>
2026-04-09 13:32:44 +0800

    spider_3额外手臂道具渲染位置与旋转调整

--- src/main/java/net/onixary/shapeShifterCurseFabric/render/tech/SpiderTPEHR.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/render/tech/SpiderTPEHR.java
@@ -11,8 +11,11 @@ import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.RotationAxis;
+import net.onixary.shapeShifterCurseFabric.ShapeShifterCurseFabric;
 
 import java.util.Map;
 import java.util.Optional;
@@ -40,15 +43,28 @@ public class SpiderTPEHR extends ThirdPersonExtraHandItemRender.TPEHR_Render {
             return;
         }
         PlayerEntityRenderer eR = (PlayerEntityRenderer) MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(player);
-        ModelPart head = eR.getModel().head;
-        matrices.translate(head.pivotX / 16.0F, head.pivotY / 16.0F, head.pivotZ / 16.0F);
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(headYaw));
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(headPitch));
-        matrices.translate(0.06F, 0.085F, -0.35D);
-        matrices.scale(1.25F, 1.25F, 1.25F);
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90.0F));
-        matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180.0F));
-        matrices.translate(1.0 / 16.0F, -2.0 / 16.0F, 1.0 / 16.0F);
-        heldItemRenderer.renderItem(player, stack, ModelTransformationMode.GROUND, false, matrices, vertexConsumers, light);
+        ModelPart body = eR.getModel().body;
+        body.rotate(matrices);
+        if(stack.getItem() == Items.SHIELD){
+            // 适用于spider_3额外手臂盾牌的transform，需要将其转向正面
+            //matrices.translate(-0.1F, 0.6F, -0.5F);
+            matrices.translate(0.1F, 0.2F, -0.2F);
+            matrices.scale(0.65F, 0.65F, 0.65F);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180.0F));
+            matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(30.0F));
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-90.0F));
+            matrices.translate(1.0 / 16.0F, -2.0 / 16.0F, 1.0 / 16.0F);
+        }
+        else{
+            // 适用于spider_3额外手臂通常道具的transform
+            matrices.translate(-0.1F, 0.6F, -0.5F);
+            matrices.scale(0.8F, 0.8F, 0.8F);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(45.0F));
+            matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180.0F));
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(15.0F));
+            matrices.translate(1.0 / 16.0F, -2.0 / 16.0F, 1.0 / 16.0F);
+        }
+        heldItemRenderer.renderItem(player, stack, ModelTransformationMode.THIRD_PERSON_RIGHT_HAND, false, matrices, vertexConsumers, light);
+
     }
 }
