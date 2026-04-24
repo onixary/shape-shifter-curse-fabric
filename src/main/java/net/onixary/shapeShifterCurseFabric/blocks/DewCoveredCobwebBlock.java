1e11d3463b521e1efa283fb0b2d4cab3ea7eb08b
onixary <undecyce@gmail.com>
2026-04-24 11:39:38 +0800

    spider变形物品与方块优化

--- src/main/java/net/onixary/shapeShifterCurseFabric/blocks/DewCoveredCobwebBlock.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/blocks/DewCoveredCobwebBlock.java
@@ -2,10 +2,14 @@ package net.onixary.shapeShifterCurseFabric.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.CobwebBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
@@ -15,12 +19,17 @@ public class DewCoveredCobwebBlock extends Block {
     }
 
     @Override
-    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        entity.slowMovement(state, new Vec3d(0.25, 0.05, 0.25));
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.empty();
     }
 
     @Override
-    public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
-        return false;
+    public float getVelocityMultiplier() {
+        return 0.25f;
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        entity.slowMovement(state, new Vec3d((double)0.25F, (double)0.05F, (double)0.25F));
     }
 }
