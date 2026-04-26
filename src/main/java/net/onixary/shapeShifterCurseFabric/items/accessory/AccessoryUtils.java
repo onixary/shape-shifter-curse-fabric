5e9eef50f485be95c05782811a80f5be785c7b76
xu233333 <18510478058@163.com>
2026-04-26 13:17:31 +0800

    现在和klit兼容了 再测一下互联

--- src/main/java/net/onixary/shapeShifterCurseFabric/items/accessory/AccessoryUtils.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/items/accessory/AccessoryUtils.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 public class AccessoryUtils {
     public static final boolean LOADED_Trinkets = FabricLoader.getInstance().isModLoaded("trinkets");
-    public static final boolean LOADED_Curios = FabricLoader.getInstance().isModLoaded("curios") && FabricLoader.getInstance().isModLoaded("connectormod");
+    public static final boolean LOADED_Curios = FabricLoader.getInstance().isModLoaded("curios");
 
     // 突然想起来我的饰品系统似乎不能处理2个相同的饰品 那还是不做同时兼容吧 这样触发概率低一些 默认使用trinkets 如果没装就试试用curios 毕竟trinkets支持的好一些
     public static String calcAutoMod(String mod) {
