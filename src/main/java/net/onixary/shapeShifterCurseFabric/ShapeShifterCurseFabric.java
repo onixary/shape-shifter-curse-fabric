7d56fb1b8037cd7ababd3ce040de7d265fa487dd
onixary <undecyce@gmail.com>
2026-06-21 23:59:24 +0800

    Tough As Nails相关形态power实现，将温度offset的插入点改到ARMOR_MODIFIERS更加合适一些；TAN的thirst_prevent_health_regen选项为true时，似乎会覆盖origins:modify_food power，之后需要找下原因

--- src/main/java/net/onixary/shapeShifterCurseFabric/ShapeShifterCurseFabric.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/ShapeShifterCurseFabric.java
@@ -48,8 +48,6 @@ import net.onixary.shapeShifterCurseFabric.form_giving_custom_entity.bat.Transfo
 import net.onixary.shapeShifterCurseFabric.form_giving_custom_entity.ocelot.TransformativeOcelotEntity;
 import net.onixary.shapeShifterCurseFabric.form_giving_custom_entity.spider.TransformativeSpiderEntity;
 import net.onixary.shapeShifterCurseFabric.form_giving_custom_entity.wolf.TransformativeWolfEntity;
-import net.onixary.shapeShifterCurseFabric.integration.toughasnails.ToughAsNailsPowerUtils;
-import net.onixary.shapeShifterCurseFabric.integration.toughasnails.ToughAsNailsTemperatureIntegration;
 import net.onixary.shapeShifterCurseFabric.blocks.RegCustomBlock;
 import net.onixary.shapeShifterCurseFabric.items.RegCustomItem;
 import net.onixary.shapeShifterCurseFabric.items.RegCustomPotions;
@@ -253,9 +251,6 @@ public class ShapeShifterCurseFabric implements ModInitializer {
             PatronUtils.OnServerLoad(server);
             TransformManager.onServerInit();
             AccessoryUtils.onStartServer();
-            if (ToughAsNailsPowerUtils.isToughAsNailsLoaded()) {
-                ToughAsNailsTemperatureIntegration.registerPlayerTemperatureModifier();
-            }
         });
         // 获取动态Form(DataPack)
         ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new FormDataPackReloadListener());
