75fe9b83b2b2f3099e8d7765e458ccecbb60bcf3
xu233333 <18510478058@163.com>
2026-09-11 19:20:28 +0800

    再更会FormUpdateScreen 目前cameraPos/cameraScale系统能正常运行 就是中心点得改一下 左上角有点影响编写perk树

--- src/main/java/net/onixary/shapeShifterCurseFabric/perk/RegPerks.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/perk/RegPerks.java
@@ -25,15 +25,23 @@ public class RegPerks {
                     .removePower()
     );
 
+    public static final Identifier P_FireBallPlusL2A = registerPerk(
+            new NormalPerk(ShapeShifterCurseFabric.identifier("fire_ball_plus_2a"))
+                    .addPower()
+                    .removePower()
+    );
+
     public static final Identifier T_FFoxTree = registerPerkTree(
             new PerkTree(ShapeShifterCurseFabric.identifier("f_fox_tree"))
                     .addNode(ShapeShifterCurseFabric.identifier("fire_ball_plus_1"), 1, 0, null)
                     .addNode(ShapeShifterCurseFabric.identifier("fire_ball_plus_2"), 2, 0, ShapeShifterCurseFabric.identifier("fire_ball_plus_1"))
+                    .addNode(ShapeShifterCurseFabric.identifier("fire_ball_plus_2a"), 2, 50, ShapeShifterCurseFabric.identifier("fire_ball_plus_1"))
     );
 
     static {
         registerPerkIcon(P_FireBallPlusL1, ShapeShifterCurseFabric.identifier("textures/perk/fire_ball_plus_1.png"));
         registerPerkIcon(P_FireBallPlusL2, ShapeShifterCurseFabric.identifier("textures/perk/fire_ball_plus_2.png"));
+        registerPerkIcon(P_FireBallPlusL2A, ShapeShifterCurseFabric.identifier("textures/perk/fire_ball_plus_1.png"));
     }
 
     public static Identifier registerPerk(IPerk perk) {
