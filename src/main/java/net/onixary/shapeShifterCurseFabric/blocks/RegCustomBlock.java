19eacb3e38cae856810bb45460c6195e60e8a3cc
xu233333 <18510478058@163.com>
2026-09-15 13:02:21 +0800

    移除2个TODO

--- src/main/java/net/onixary/shapeShifterCurseFabric/blocks/RegCustomBlock.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/blocks/RegCustomBlock.java
@@ -22,7 +22,6 @@ import net.onixary.shapeShifterCurseFabric.blocks.block_entity.FormAttunerBlockE
 
 public final class RegCustomBlock {
     public static final Block MOONDUST_CRYSTAL_GRIT = register("moondust_crystal_grit", new Block(AbstractBlock.Settings.copy(Blocks.GRAVEL).mapColor(MapColor.PURPLE).strength(0.6f, 0.6f).sounds(BlockSoundGroup.GRAVEL)));
-    // TODO TEMP_WEB_BRIDGE 仅在测试时有物品 发布时记得用 registerWithOutItem
     public static final Block TEMP_WEB_BRIDGE = register("temp_web_bridge", new TempWebBridgeBlock(AbstractBlock.Settings.create().mapColor(MapColor.WHITE_GRAY).strength(4.0f).ticksRandomly().noCollision().dynamicBounds().dropsNothing().solidBlock(Blocks::never).burnable().sounds(BlockSoundGroup.WOOL)));
 
     public static final Block WEB_COMPOSTER = register("web_composter", new WebComposterBlock(AbstractBlock.Settings.create().mapColor(MapColor.WHITE_GRAY).instrument(Instrument.BASS).strength(0.6F).sounds(BlockSoundGroup.WOOL).burnable().nonOpaque()));
